d656d48b6895854356358cc08058e5913edb95ee
Matt <mshamp4@users.noreply.github.com>
2017-09-23 11:27:12 -0400

    Create file_utils.h

---
+++ file_utils.h
@@ -0,0 +1,11 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+/**
+* 
+*/
+int read_file(char* filename, char** buffer);
+
+int write_file(char* filename, char* buffer, int size);
+
+#endif /* FILE_UTILS_H */
