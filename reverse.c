abb6a8f83279684c1b0dccd8cb9181dc09bdd894
Matt <mshamp4@users.noreply.github.com>
2017-09-23 18:08:16 -0400

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -3,21 +3,21 @@
 #include "file_utils.h"
 
 int main(int argc, char** argv){
-	if (argc != 3) {
-		fprintf(stderr, "Error! invalid arguments\nUsage: %s [input file] " 
-		"[output file]", argv[0]);
-		return 1;
-	}	
-	
-	char* buffer;
-	read_file(argv[1], &buffer);
-	
-	for (int i = 0; i < 4; i++) {
-		printf("%c", buffer[i]);
-	}
-	
-	free(buffer);
-	//write_file(char* filename, char* buffer, int size);
-	
-	return 0;
+        if (argc != 3) {
+                fprintf(stderr, "Error! invalid arguments\nUsage: %s [input file] "
+                "[output file]\n", argv[0]);
+                return 1;
+        }
+
+        char* buffer;
+        int size = read_file(argv[1], &buffer);
+
+        for (int i = size; i >= 0; i--) {
+                printf("%c", buffer[i]);
+        }
+
+        write_file(argv[2], buffer, size);
+
+        free(buffer);
+        return 0;
 }
