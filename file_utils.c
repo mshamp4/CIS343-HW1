b4c8dcd5d47c2aa95830e497fc78a6b111b1c060
Matt <mshamp4@users.noreply.github.com>
2017-09-23 18:07:09 -0400

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -3,33 +3,52 @@
 #include "file_utils.h"
 
 int read_file(char* filename, char** buffer) {
-	FILE *file = fopen(filename, "r");
-	if (file == NULL) {
-		return 1;
-	}
-	fseek(file, 0, SEEK_END);
-	long size = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	// TO-DO: make sure memory call completes successfully
-	*buffer = (char*)malloc(sizeof(char) * size + 1);
- 
-//	char value = NULL;
-	
-	fread(*buffer, sizeof(char), size, file);
-	
-	
-//	for (int i = 0; value != EOF; i++) {
-//		value = fgetc(file);
-//		printf("%c", value);
-//		buffer[i] = &value; // error here
-//	}
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+                return 1;
+        }
+        fseek(file, 0L, SEEK_END);
+        long size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+        printf("%lu", sizeof(char));
+//      printf("%li\n", size);
+//      int size1 = sizeof(char) * size;
+//      printf("%i\n", size1);
+        // TO-DO: make sure memory call completes successfully
+        *buffer = (char*)malloc(sizeof(char) * size);
 
-	if (fclose(file) != 0) {
+//      char value = NULL;
+
+        fread(*buffer, sizeof(char), size, file);
+
+
+//      for (int i = 0; value != EOF; i++) {
+//              value = fgetc(file);
+//              printf("%c", value);
+//              buffer[i] = &value; // error here
+//      }
+
+        if (fclose(file) != 0) {
 		return 1;
-	}
-	return size;
+        }       
+        return size;
 }
 
 int write_file(char* filename, char* buffer, int size) {
-	return 0;
+        FILE *wrtFile = fopen(filename, "w");
+//      printf("%i", size);
+//      size = size - 2;
+        while (size >= 0) {
+                fputc(buffer[size], wrtFile);
+                size--;
+        }
+//      for (int i = size; size > 0; i--) {
+//              printf("%i", i);
+//              fputc(buffer[10], wrtFile);
+//      }
+
+
+        return 0;
 }
+
+
