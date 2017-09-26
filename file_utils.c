24e94537d460bbc2423c2d788a1c9272107afb41
Matt <mshamp4@users.noreply.github.com>
2017-09-26 15:50:55 -0400

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -7,26 +7,17 @@ int read_file(char* filename, char** buffer) {
         if (file == NULL) {
                 return -1;
         }
+		
         fseek(file, 0L, SEEK_END);
         long size = ftell(file);
         fseek(file, 0L, SEEK_SET);
-//        printf("%lu", sizeof(char));
-//      printf("%li\n", size);
-//      int size1 = sizeof(char) * size;
-//      printf("%i\n", size1);
+		
+//		printf("Size of file: %ld", size);
+
         // TO-DO: make sure memory call completes successfully
         *buffer = (char*)malloc(sizeof(char) * size);
 
-//      char value = NULL;
-
         fread(*buffer, sizeof(char), size, file);
-
-
-//      for (int i = 0; value != EOF; i++) {
-//              value = fgetc(file);
-//              printf("%c", value);
-//              buffer[i] = &value; // error here
-//      }
 
         if (fclose(file) != 0) {
 			return -1;
@@ -36,14 +27,18 @@ int read_file(char* filename, char** buffer) {
 
 int write_file(char* filename, char* buffer, int size) {
         FILE *wrtFile = fopen(filename, "w");
+		size = size;
 		
-		for (int i = 0; i < size; i++) {
-			fputc(buffer[i], wrtFile);
+		printf("Size: %i", size);
+		
+		int count = 0;
+		while (count < size) {
+			fputc(buffer[count], wrtFile);
+			count++;
 		}
-		
+
 		if (fclose(wrtFile) != 0) {
 			return -1;
         }
         return 0;
 }
-
