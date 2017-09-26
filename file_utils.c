fcd7f5ed2e94688ed015f89a1f0d3d01bf46f41c
Matt <mshamp4@users.noreply.github.com>
2017-09-26 14:42:24 -0400

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -5,12 +5,12 @@
 int read_file(char* filename, char** buffer) {
         FILE *file = fopen(filename, "r");
         if (file == NULL) {
-                return 1;
+                return -1;
         }
         fseek(file, 0L, SEEK_END);
         long size = ftell(file);
         fseek(file, 0L, SEEK_SET);
-        printf("%lu", sizeof(char));
+//        printf("%lu", sizeof(char));
 //      printf("%li\n", size);
 //      int size1 = sizeof(char) * size;
 //      printf("%i\n", size1);
@@ -29,26 +29,21 @@ int read_file(char* filename, char** buffer) {
 //      }
 
         if (fclose(file) != 0) {
-		return 1;
+			return -1;
         }       
         return size;
 }
 
 int write_file(char* filename, char* buffer, int size) {
         FILE *wrtFile = fopen(filename, "w");
-//      printf("%i", size);
-        size = size - 2;
-        while (size >= 0) {
-                fputc(buffer[size], wrtFile);
-                size--;
+		
+		for (int i = 0; i < size; i++) {
+			fputc(buffer[i], wrtFile);
+		}
+		
+		if (fclose(wrtFile) != 0) {
+			return -1;
         }
-//      for (int i = size; size > 0; i--) {
-//              printf("%i", i);
-//              fputc(buffer[10], wrtFile);
-//      }
-
-
         return 0;
 }
-
 
