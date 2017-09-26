101612218b642efc68e28f6220145361bf6eb530
Matt <mshamp4@users.noreply.github.com>
2017-09-26 16:39:23 -0400

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -4,15 +4,14 @@
 
 int read_file(char* filename, char** buffer) {
         FILE *file = fopen(filename, "r");
+
         if (file == NULL) {
                 return -1;
         }
-		
+
         fseek(file, 0L, SEEK_END);
         long size = ftell(file);
         fseek(file, 0L, SEEK_SET);
-		
-//		printf("Size of file: %ld", size);
 
         // TO-DO: make sure memory call completes successfully
         *buffer = (char*)malloc(sizeof(char) * size);
@@ -20,25 +19,8 @@ int read_file(char* filename, char** buffer) {
         fread(*buffer, sizeof(char), size, file);
 
         if (fclose(file) != 0) {
-			return -1;
-        }       
-        return size;
-}
-
-int write_file(char* filename, char* buffer, int size) {
-        FILE *wrtFile = fopen(filename, "w");
-		size = size;
-		
-		printf("Size: %i", size);
-		
-		int count = 0;
-		while (count < size) {
-			fputc(buffer[count], wrtFile);
-			count++;
-		}
-
-		if (fclose(wrtFile) != 0) {
-			return -1;
+                return -1;
         }
-        return 0;
+
+        return size;
 }
