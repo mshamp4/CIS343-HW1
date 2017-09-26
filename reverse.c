f4069524aff2b75d64bbe3585c751ebeec095b45
Matt <mshamp4@users.noreply.github.com>
2017-09-26 14:41:59 -0400

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -11,12 +11,36 @@ int main(int argc, char** argv){
 
         char* buffer;
         int size = read_file(argv[1], &buffer);
+		
+		if (size == -1) {
+			fprintf(stderr, "Error reading file!\n");
+			return 1;
+		}
+		
+//		char* endBuff = &buffer[size - 1];
+		
+//		printf("Char:%c", *endBuff);
 
-        for (int i = size; i >= 0; i--) {
-                printf("%c", buffer[i]);
-        }
+//        for (int i = (size - 1); i >= 0; i--) {
+//                printf("%c", buffer[i]);
+//        }
 
-        write_file(argv[2], buffer, size);
+		int end = size - 1;
+		int start = 0;
+		
+		while (end > start) {
+			char temp = buffer[end];
+			buffer[end] = buffer[start];
+			buffer[start] = temp;
+			end--;
+			start++;
+		}
+		
+//		for (int i = 0; i < size; i++) {
+//			printf("%c", buffer[i]);
+//		}
+
+		write_file(argv[2], buffer, size);
 
         free(buffer);
         return 0;
