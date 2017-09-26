d75e9ee91318b0b54997f645cabb38d5c7d57986
Matt <mshamp4@users.noreply.github.com>
2017-09-26 16:41:25 -0400

    Update file_utils.c

--- file_utils.c
+++ file_utils.c
@@ -2,25 +2,34 @@
 #include <stdlib.h>
 #include "file_utils.h"
 
-int read_file(char* filename, char** buffer) {
-        FILE *file = fopen(filename, "r");
-
-        if (file == NULL) {
-                return -1;
+int main(int argc, char** argv){
+        if (argc != 3) {
+                fprintf(stderr, "Error! invalid arguments\nUsage: %s [input file] "
+                "[output file]\n", argv[0]);
+                return 1;
         }
 
-        fseek(file, 0L, SEEK_END);
-        long size = ftell(file);
-        fseek(file, 0L, SEEK_SET);
+        char* buffer;
+        int size = read_file(argv[1], &buffer);
 
-        // TO-DO: make sure memory call completes successfully
-        *buffer = (char*)malloc(sizeof(char) * size);
-
-        fread(*buffer, sizeof(char), size, file);
-
-        if (fclose(file) != 0) {
-                return -1;
+        if (size == -1) {
+                fprintf(stderr, "Error reading file!\n");
+                return 1;
         }
 
-        return size;
+        int end = size - 1;
+        int start = 0;
+
+        while (end > start) {
+                char temp = buffer[end];
+                buffer[end] = buffer[start];
+                buffer[start] = temp;
+                end--;
+                start++;
+        }
+
+        write_file(argv[2], buffer, size);
+
+        free(buffer);
+        return 0;
 }
