064319ae7dd976596738d8cfec0a4a299118e743
Matt <mshamp4@users.noreply.github.com>
2017-09-26 16:40:28 -0400

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -2,38 +2,25 @@
 #include <stdlib.h>
 #include "file_utils.h"
 
-int main(int argc, char** argv){
-        if (argc != 3) {
-                fprintf(stderr, "Error! invalid arguments\nUsage: %s [input file] "
-                "[output file]\n", argv[0]);
-                return 1;
+int read_file(char* filename, char** buffer) {
+        FILE *file = fopen(filename, "r");
+
+        if (file == NULL) {
+                return -1;
         }
 
-        char* buffer;
-        int size = read_file(argv[1], &buffer);
-		
-		if (size == -1) {
-			fprintf(stderr, "Error reading file!\n");
-			return 1;
-		}
-		
-		int end = size - 1;
-		int start = 0;
-		
-//		while (end > start) {
-//			char temp = buffer[end];
-//			buffer[end] = buffer[start];
-//			buffer[start] = temp;
-//			end--;
-//			start++;
-//		}
-		
-		for (int i = 0; i < size; i++) {
-			printf("%c", buffer[i]);
-		}
+        fseek(file, 0L, SEEK_END);
+        long size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
 
-//		write_file(argv[2], buffer, size);
+        // TO-DO: make sure memory call completes successfully
+        *buffer = (char*)malloc(sizeof(char) * size);
 
-        free(buffer);
-        return 0;
+        fread(*buffer, sizeof(char), size, file);
+
+        if (fclose(file) != 0) {
+                return -1;
+        }
+	
+        return size;
 }
