b9d8549a3492f6254c034e1bcdcbc6b9148ff19d
Matt <mshamp4@users.noreply.github.com>
2017-09-26 22:49:24 -0400

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -2,25 +2,46 @@
 #include <stdlib.h>
 #include "file_utils.h"
 
-int read_file(char* filename, char** buffer) {
-        FILE *file = fopen(filename, "r");
-
-        if (file == NULL) {
-                return -1;
+/**
+ * This program takes an input file, reverses 
+ * the contents and writes it to an output file.
+ */
+int main(int argc, char** argv){
+        if (argc != 3) {
+                fprintf(stderr, "Error! invalid arguments\nUsage: %s [input file] "
+                "[output file]\n", argv[0]);
+                return 1;
         }
 
-        fseek(file, 0L, SEEK_END);
-        long size = ftell(file);
-        fseek(file, 0L, SEEK_SET);
+        char* buffer;
+        int size = read_file(argv[1], &buffer);
 
-        // TO-DO: make sure memory call completes successfully
-        *buffer = (char*)malloc(sizeof(char) * size);
-
-        fread(*buffer, sizeof(char), size, file);
-
-        if (fclose(file) != 0) {
-                return -1;
+        if (size == -1) {
+                fprintf(stderr, "Error reading file!\n");
+                free(buffer);
+                return 1;
         }
-	
-        return size;
+
+        // Put this into a seperate method              
+        int end = size - 1;
+        int start = 0;
+
+        while (end > start) {
+                char temp = buffer[end];
+                buffer[end] = buffer[start];
+                buffer[start] = temp;
+                end--;
+                start++;
+        }
+
+        int success = write_file(argv[2], buffer, size);
+
+        if (success != 0) {
+                fprintf(stderr, "Error writing file!\n");
+                free(buffer);
+                return 1;
+        }
+
+        free(buffer);
+        return 0;
 }
